6c74bd57203c6ab6f8975b3a606a58a16ccee8d0
eduardogushiken <eduardogushiken13@gmail.com>
2015-10-07 02:43:58 -0300

    3.26 Botão Share

--- app/src/main/java/com/example/usuario/sunshinev3/DetailActivityFragment.java
+++ app/src/main/java/com/example/usuario/sunshinev3/DetailActivityFragment.java
@@ -1,12 +1,23 @@
 package com.example.usuario.sunshinev3;
 
 import android.content.Intent;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+//import android.widget.ShareActionProvider;
 import android.widget.TextView;
+
+//Lição 3.26
+import android.support.v7.widget.ShareActionProvider;
+
+import java.util.zip.Inflater;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -14,7 +25,15 @@ import android.widget.TextView;
 public class DetailActivityFragment extends Fragment {
 
     public DetailActivityFragment() {
+        //Lição 3.26
+        setHasOptionsMenu(true);
     }
+
+    //Lição 3.26
+    private static final String LOG_TAG = DetailActivityFragment.class.getSimpleName();
+
+    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+    private String mForecastStr;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -25,11 +44,37 @@ public class DetailActivityFragment extends Fragment {
         //Lição 3.11 Inserindo texto na detailActivity
         Intent intent = getActivity().getIntent();
         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-            String forecstStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            //Lição 3.26
+            //String forecstStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+            mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
             ((TextView) rootView.findViewById(R.id.detail_text))
-                    .setText(forecstStr);
+                    .setText(mForecastStr);
+                    //.setText(forecstStr);
         }
 
         return rootView;
+    }
+
+    //Lição 3.26
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+        inflater.inflate(R.menu.menu_detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        ShareActionProvider mShareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        if (mShareActionProvider != null) {
+            mShareActionProvider.setShareIntent(createShareForecastIntent());
+        } else {
+            Log.d(LOG_TAG, "Share Action Provider is null?");
+        }
+    }
+
+    private Intent createShareForecastIntent() {
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT,
+                mForecastStr + FORECAST_SHARE_HASHTAG);
+        return shareIntent;
     }
 }
